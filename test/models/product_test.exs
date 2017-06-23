64364abe81da39aa9cdfb206096b734ea6237051
Jason Cartwright <jcartwright@Jasons-MBP.fios-router.home>
2017-06-23 14:58:07 -0500

    Implement Product.update_price!/2
    - updates Product when there is a valid price change
    - fails on negative price value
    - creates PastPriceRecord when there is a valid price change
    - calculates the percentage change in prices

--- test/models/product_test.exs
+++ test/models/product_test.exs
@@ -1,9 +1,9 @@
 defmodule Telnyx.ProductTest do
   use Telnyx.ModelCase
 
-  alias Telnyx.Product
+  alias Telnyx.{Repo, Product, PastPriceRecord}
 
-  @valid_attrs %{external_product_id: "some content", price_in_cents: 42, product_name: "some content"}
+  @valid_attrs %{external_product_id: "AWv123", price_in_cents: 10000, product_name: "Acme Widget v1.2.3"}
   @invalid_attrs %{}
 
   test "changeset with valid attributes" do
@@ -14,5 +14,81 @@ defmodule Telnyx.ProductTest do
   test "changeset with invalid attributes" do
     changeset = Product.changeset(%Product{}, @invalid_attrs)
     refute changeset.valid?
+  end
+
+  test "price_update_changeset/2 with price change is valid"  do
+    price_change_attrs = %{price_in_cents: 4301}
+    changeset = Product.price_update_changeset(%Product{}, price_change_attrs)
+    assert changeset.valid?
+    assert %{price_in_cents: _} = changeset.changes
+  end
+
+  describe "update_price!/2" do
+    setup do
+      product =
+        %Product{}
+        |> Product.changeset(@valid_attrs)
+        |> Repo.insert!()
+
+      {:ok, product: product}
+    end
+
+    test "with no price change does not create a past_price_record", %{product: product} do
+      product =
+        product
+        |> Product.update_price!(product.price_in_cents)
+        |> Repo.preload(:past_price_records)
+
+      assert Enum.empty?(product.past_price_records)
+    end
+
+    test "with a price change updates product's price", %{product: product} do
+      original_price = product.price_in_cents
+      new_price = original_price + 1000
+
+      product = Product.update_price!(product, new_price)
+      assert new_price == product.price_in_cents
+    end
+
+    test "with price change creates a past_price_record on update", %{product: product} do
+      product =
+        product
+        |> Product.update_price!(product.price_in_cents + 1000)
+        |> Repo.preload(:past_price_records)
+
+      refute Enum.empty?(product.past_price_records)
+    end
+
+    test "with price change archives the percentage_change", %{product: product} do
+      product =
+        product
+        |> Product.update_price!(product.price_in_cents + 1100)
+        |> Repo.preload(:past_price_records)
+
+      %PastPriceRecord{percentage_change: percentage_change}  =
+        product.past_price_records
+        |> List.last()
+
+      assert 10.0 == Float.round(percentage_change)
+    end
+
+    test "with price change archives the original price", %{product: product} do
+      original_price = product.price_in_cents
+
+      product =
+        product
+        |> Product.update_price!(original_price + 1100)
+        |> Repo.preload(:past_price_records)
+
+      assert %PastPriceRecord{price_in_cents: ^original_price} =
+        product.past_price_records
+        |> List.last()
+    end
+
+    test "with a negative price, raises an exception", %{product: product} do
+      assert_raise FunctionClauseError, ~r/no function clause matching/, fn ->
+        product |> Product.update_price!(-100)
+      end
+    end
   end
 end
--- web/models/product.ex
+++ web/models/product.ex
@@ -1,5 +1,6 @@
 defmodule Telnyx.Product do
   use Telnyx.Web, :model
+  alias Telnyx.{Repo, Product, PastPriceRecord}
 
   schema "products" do
     field :external_product_id, :string
@@ -10,6 +11,31 @@ defmodule Telnyx.Product do
     timestamps()
   end
 
+  def update_price!(product, price) when is_integer(price) and price >= 0 do
+    current_price = product.price_in_cents
+    new_price = price
+
+    cond do
+      current_price == new_price ->
+        product
+      true ->
+        price_changeset = 
+          %PastPriceRecord{}
+          |> PastPriceRecord.changeset(
+              %{
+                price_in_cents: current_price,
+                percentage_change: calculate_percentage_change(current_price, new_price)
+              })
+
+        product_changeset = 
+          product
+          |> Repo.preload(:past_price_records)
+          |> Product.price_update_changeset(%{price_in_cents: new_price})
+          |> Ecto.Changeset.put_assoc(:past_price_records, [price_changeset])
+          |> Repo.update!()
+    end
+  end
+
   @doc """
   Builds a changeset based on the `struct` and `params`.
   """
@@ -18,4 +44,19 @@ defmodule Telnyx.Product do
     |> cast(params, [:external_product_id, :price_in_cents, :product_name])
     |> validate_required([:external_product_id, :price_in_cents, :product_name])
   end
+
+  @doc """
+  Builds a price update changeset based on the `struct` and `params`, only
+  when the price_in_cents is different from the current value.
+  """
+  def price_update_changeset(struct, %{price_in_cents: price_in_cents} = params)
+    when is_integer(price_in_cents) and price_in_cents >= 0 do
+    struct
+    |> cast(params, [:price_in_cents])
+    |> validate_required([:price_in_cents])
+  end
+
+  defp calculate_percentage_change(original_price, new_price)
+    when is_integer(original_price) and is_integer(new_price),
+    do: ((new_price - original_price) / new_price) * 100
 end
